e2f600b235d7df09f9ec8f2efcab59323d2b8d8b
dapanjimianpaigumian <zhao_xin_peng@hotmail.com>
2017-03-31 01:07:46 +0800

    实现自定义通知栏样式、自定义广播接收器、推送带字段的通知消息

--- app/src/main/java/com/yulu/zhaoxinpeng/myjiguangdemo/MainActivity.java
+++ app/src/main/java/com/yulu/zhaoxinpeng/myjiguangdemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.yulu.zhaoxinpeng.myjiguangdemo;
 
+import android.app.Notification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.CustomPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.MultiActionsNotificationBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+
+        //设置自定义通知栏样式1
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(MainActivity.this);
+        //指定状态栏的小图标
+        builder.statusBarDrawable = R.drawable.dog;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL
+                | Notification.FLAG_SHOW_LIGHTS;  //设置为自动消失和呼吸灯闪烁
+        builder.notificationDefaults = Notification.DEFAULT_SOUND
+                | Notification.DEFAULT_VIBRATE
+                | Notification.DEFAULT_LIGHTS;  // 设置为铃声、震动、呼吸灯闪烁都要
+        //设置通知栏编号，样式
+        JPushInterface.setPushNotificationBuilder(1,builder);
+
+
+        //设置自定义通知栏样式2
+        MultiActionsNotificationBuilder builder2 = new MultiActionsNotificationBuilder(MainActivity.this);
+        //添加按钮，参数(按钮图片、按钮文字、扩展数据)
+        builder2.addJPushAction(R.drawable.jpush_ic_richpush_actionbar_back, "first", "my_extra1");
+        builder2.addJPushAction(R.drawable.jpush_ic_richpush_actionbar_back, "second", "my_extra2");
+        builder2.addJPushAction(R.drawable.jpush_ic_richpush_actionbar_back, "third", "my_extra3");
+        JPushInterface.setPushNotificationBuilder(2, builder2);
+
+        //设置自定义通知栏样式3
+        CustomPushNotificationBuilder builder3 = new
+                CustomPushNotificationBuilder(MainActivity.this,
+                R.layout.view_notification,
+                R.id.icon,
+                R.id.title,
+                R.id.text);
+        //指定状态栏的小图标
+        builder3.statusBarDrawable = R.drawable.dog;
+        // 指定最顶层状态栏小图标
+        builder3.layoutIconDrawable = R.drawable.yellow;
+        // 指定下拉状态栏时显示的通知图标
+        JPushInterface.setPushNotificationBuilder(3, builder3);
     }
 }
---
+++ app/src/main/java/com/yulu/zhaoxinpeng/myjiguangdemo/MyReceiver.java
@@ -0,0 +1,125 @@
+package com.yulu.zhaoxinpeng.myjiguangdemo;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+import cn.jpush.android.api.JPushInterface;
+
+/**
+ * Created by Administrator on 2017/3/31.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "MyReceiver";
+
+    private NotificationManager nm;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (null == nm) {
+            nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        Bundle bundle = intent.getExtras();
+        Log.d(TAG, "onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+
+        if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+            Log.d(TAG, "JPush用户注册成功");
+
+        } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+            Log.d(TAG, "接受到推送下来的自定义消息");
+
+            Toast.makeText(context, "收到了自定义消息", Toast.LENGTH_SHORT).show();
+
+        } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+            Log.d(TAG, "接受到推送下来的通知");
+
+            Toast.makeText(context, "收到了通知", Toast.LENGTH_SHORT).show();
+
+            receivingNotification(context,bundle);
+
+            openNotification(context,bundle);
+
+        } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
+            Log.d(TAG, "用户点击打开了通知");
+
+            Toast.makeText(context, "用户点击了通知", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Log.d(TAG, "Unhandled intent - " + intent.getAction());
+        }
+    }
+
+    private void receivingNotification(Context context, Bundle bundle){
+        String title = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
+        Log.d(TAG, " title : " + title);
+        String message = bundle.getString(JPushInterface.EXTRA_ALERT);
+        Log.d(TAG, "message : " + message);
+        String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
+        Log.d(TAG, "extras : " + extras);
+    }
+
+    private void openNotification(Context context, Bundle bundle){
+        String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
+        String myValue = "";
+        try {
+            JSONObject extrasJson = new JSONObject(extras);
+            //值                             键
+            myValue = extrasJson.optString("key");
+        } catch (Exception e) {
+            Log.w(TAG, "Unexpected: extras is not a valid json", e);
+            return;
+        }
+        //键 Key  值  123    幼儿园
+        if (myValue.equals("123")){
+            Toast.makeText(context, "幼儿园~！", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "不是幼儿园！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // 打印所有的 intent extra 数据
+    private static String printBundle(Bundle bundle) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_NOTIFICATION_ID)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getInt(key));
+            }else if(key.equals(JPushInterface.EXTRA_CONNECTION_CHANGE)){
+                sb.append("\nkey:" + key + ", value:" + bundle.getBoolean(key));
+            } else if (key.equals(JPushInterface.EXTRA_EXTRA)) {
+                if (TextUtils.isEmpty(bundle.getString(JPushInterface.EXTRA_EXTRA))) {
+                    Log.i(TAG, "This message has no Extra data");
+                    continue;
+                }
+
+                try {
+                    JSONObject json = new JSONObject(bundle.getString(JPushInterface.EXTRA_EXTRA));
+                    Iterator<String> it =  json.keys();
+
+                    while (it.hasNext()) {
+                        String myKey = it.next().toString();
+                        sb.append("\nkey:" + key + ", value: [" +
+                                myKey + " - " +json.optString(myKey) + "]");
+                    }
+                } catch (JSONException e) {
+                    Log.e(TAG, "Get message extra JSON error!");
+                }
+
+            } else {
+                sb.append("\nkey:" + key + ", value:" + bundle.getString(key));
+            }
+        }
+        return sb.toString();
+    }
+}
